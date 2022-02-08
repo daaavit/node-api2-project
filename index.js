594509ab6a01da9b626b51cc2e012bcfba3aeba4
jabaracconich <87547340+jabaracconich@users.noreply.github.com>
2022-02-08 13:58:48 -0500

    a

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // require your server and launch it here
+
+
+const server= require('./api/server')
+
+const PORT = 9000
+
+server.listen(PORT, () => {
+    console.log('server listens on 9000', PORT)
+})
