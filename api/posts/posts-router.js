c4b931afa60c1e897a8873e7f045f58100da56b6
jabaracconich <87547340+jabaracconich@users.noreply.github.com>
2022-02-08 15:41:21 -0500

    e

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,33 +1,52 @@
 // implement your posts router here
 
 const express = require('express')
+const Post = require('./posts-model')
 
 const router = express.Router()
 
 
 router.get('/', (req, res) => {
-    res.json('foo')
+    Post.find()
+        .then(found => {
+            res.json(found) 
+
+        })
+        .chatch(err => {
+            res.status(500).json({
+                message: "The post information could not be retrieved",
+                err: err.message,
+                stack: err.stack,
+            })
+        })
+
 
 })
 
-router.get('/:id', (req, res) => {
-    
+router.get('/:id', async (req, res) => {
+    try {
+
+    }
+    caches( err ) {
+
+    }
+
 })
 
 router.post('/', (req, res) => {
-    
+
 })
 
 router.delete('/:id', (req, res) => {
-    
+
 })
 
 router.put('/:id', (req, res) => {
-    
+
 })
 
 router.get('/:id/message', (req, res) => {
-    
+
 })
 
 
